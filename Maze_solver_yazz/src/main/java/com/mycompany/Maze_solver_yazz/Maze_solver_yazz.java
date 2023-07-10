8b5eae3e7398cf296e20ae5a199170fc221e4e7b
Md Yazdan Rizwan <85094744+YazdanRizwan@users.noreply.github.com>
2023-07-11 00:54:00 +0530

    Add files via upload

---
+++ Maze_solver_yazz/src/main/java/com/mycompany/Maze_solver_yazz/Maze_solver_yazz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.Maze_solver_yazz;
+
+/**
+ *
+ * @author Yazdan
+ */
+public class Maze_solver_yazz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
